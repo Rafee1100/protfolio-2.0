6d35eaf4dc8190a2c6b4661633024449287aaafd
pmvagadiya <parthmvagadiya001@gmail.com>
2021-05-03 21:37:38 +0600

    first commit

---
+++ src/utils/project-list.js
@@ -0,0 +1,79 @@
+import travelImg from '../utils/travel.png'
+import footballImg from '../utils/football.png';
+import Ecommerce from '../utils/Ecommerce.png'
+import pandaImg from '../utils/Panda.png'
+import foodmania from '../utils/foodmania.png'
+
+let project_list = [
+	{
+		image:travelImg,
+		title: 'Travel Hub',
+		des:
+			"Full Stack Project using ReactJS and FIREBASE Development of new highly-responsive, web-based user interface Online payment implementation with stripe.",
+		tags: [ 'React Js', 'Tailwind Css','Node js','Express js','Firebase', 'JavaScript' ],
+		links: {
+			launch: 'https://travel1100-769d2.web.app/',
+			github: 'https://github.com/Rafee1100/travel-agent'
+		}
+	},
+	{
+		image:footballImg,
+		title: 'Premier League Team Management',
+		des:
+			"Premier League Team Management. Knowledge about all the teams playing premier league.Described Details",
+		tags: [ 'React js', 'bootstrap', 'Javascript'],
+		links: {
+			launch: 'https://football-adda-1100.netlify.app/',
+			github: 'https://github.com/Rafee1100/react-team-management'
+		}
+	},
+	{  /* -------------------------------- */
+		image:Ecommerce,
+		title: 'E-Commerce Website',
+		des:
+			"An E-Commerce application. The products of which are basically Shoes",
+		tags: ['HTML','CSS', 'JavaScript' ],
+		links: {
+			launch: 'https://rafee1100.github.io/Ecommerce-shoe/',
+			github: 'https://github.com/Rafee1100/Ecommerce-shoe'
+		}
+	},
+	{/* -------------------------------- */
+		image:
+			foodmania,
+		title: 'E-shop',
+		des:
+			"E-Shop for making your daily life Easy. Now the Bazar is at your door.",
+		tags: [ 'HTML', 'Bootstrap', 'JavaScript', 'React','Node.Js' ],
+		links: {
+			launch: 'https://e-commerce-product-3b61a.web.app/',
+			github: 'https://github.com/Porgramming-Hero-web-course/full-stack-client-Rafee1100'
+		}
+	},
+	{/* -------------------------------- */
+		image:
+			pandaImg,
+		title: 'Panda Shop',
+		des:
+			"",
+		tags: [ 'HTML', 'CSS', 'JavaScript' ],
+		links: {
+			launch: 'https://rafee1100.github.io/panda-ecommerce/',
+			github: 'https://github.com/Rafee1100/panda-ecommerce'
+		}
+	},
+	// {/* -------------------------------- */
+	// 	image:
+	// 		'https://drive.google.com/uc?id=1DXb4hG4d3MDNJA0iBclYGC4JJJU53xzw',
+	// 	title: 'JavaScript Clock',
+	// 	des:
+	// 		"Javascript Clock is an application built with VanillaJS , where local time is fetched and shown in a clock. It has hour, minute and second hands",
+	// 	tags: ['HTML', 'CSS', 'JavaScript' ],
+	// 	links: {
+	// 		launch: 'https://faizhameed.github.io/JavascriptClock/',
+	// 		github: 'https://github.com/faizhameed/JavascriptClock'
+	// 	}
+	// }
+];
+
+export default project_list;
